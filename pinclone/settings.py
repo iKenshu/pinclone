573d24a7956bfed4b53c858c07ab015885f2c819
ikenshu <undefined.hlo.o@gmail.com>
2017-09-04 14:21:29 -0400

    change the database

--- pinclone/settings.py
+++ pinclone/settings.py
@@ -128,9 +128,5 @@ except ImportError:
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
